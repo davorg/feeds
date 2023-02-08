f754a0af3097591c62fa09d3ecdf0a2df680fce6
Dave Cross <dave@dave.org.uk>
2023-02-08 15:54:34 +0000

    Add some TODOs

--- Feeds/lib/Feeds.pm
+++ Feeds/lib/Feeds.pm
@@ -18,29 +18,39 @@ get '/' => sub {
 };
 
 get '/:feed' => sub {
-  my $feed = route_parameters->get('feed');
+  my $feed_name = route_parameters->get('feed');
+  my $feed;
 
-  if (exists config->{feeds}{$feed}) {
-    $feed = config->{feeds}{$feed};
+  if (exists config->{feeds}{$feed_name}) {
+    $feed = config->{feeds}{$feed_name};
   } else {
     status(404);
-    return "$feed is not a known feed";
+    return "$feed_name is not a known feed";
   }
 
+  # TODO: Only set the content type once we know it's
+  # a valid feed and (more importantly) what the charset is
   content_type "application/$feed->{feed}+xml; charset=UTF-8";
   response_header 'Access-Control-Allow-Origin' => '*';
 
+  # TODO: Dispatch table
   if ($feed->{type} eq 'file') {
+    # TODO: This decode/encode seems pointless
     my $data = decode 'UTF-8', path($feed->{path})->slurp_utf8;
     return encode 'UTF-8', $data;
   }
 
   if ($feed->{type} eq 'uri') {
+    # TODO: Is there any point in decoding the data in the sub
+    # only to encode it again here?
     return encode 'UTF-8', get_uri($feed->{uri});
   }
 
+  # TODO: Should this die, not warn?
   warn "Unknown feed type: $feed->{type}";
 
+  # TODO: What should we really return here?
+  # TODO: Use HTTP::Exception?
   return $feed;
 };
 
@@ -49,13 +59,16 @@ true;
 sub get_uri {
   my ($uri) = shift;
 
+  # TODO: Persist the UA under PSGI?
   my $ua = LWP::UserAgent->new( agent => "Dave's Feed Engine" );
   my $resp = $ua->get($uri);
 
+  # TODO: Throw an HTTP::Exception here?
   if (! $resp->is_success) {
     die $resp->status_line;
     return;
   }
 
+  # TODO: This is pointless if we just encode the data again
   return decode 'UTF-8', $resp->content;
 }
